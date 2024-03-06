ab8108512a592bf079e7e905b1c13d4906ec02e6
MinieMoe <minnguyen7.2001@gmail.com>
2024-03-05 21:39:28 -0800

    bug

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
+
 from GitHubService import GitHubService 
 
 def main():
-    token = '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'
+    token = os.getenv('GITHUB_ACCESS_TOKEN')
     repo_name ='ReaVNaiL/New-Grad-2024'
     github_service = GitHubService(token,repo_name)
 
-    github_service.parse_job_info_from_latest_commit
+    github_service.parse_job_info_from_latest_commit()
     
 if __name__ == '__main__':
     # run the  bot
