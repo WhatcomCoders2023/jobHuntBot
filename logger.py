09a0c9a1500eca8773316d75418fd8a2f9efeb62
Tin Ngo <tin159iop@yahoo.com>
2024-04-30 08:31:32 -0700

    Make code more readable

--- logger.py
+++ logger.py
@@ -1,26 +1,24 @@
 import logging
 import os
+import google.cloud.logging
 
 
 def init_logger():
-    # Prod mode
-    if not os.getenv("DEV_MODE"):
-        import google.cloud.logging
-
-        client = google.cloud.logging.Client()
-        client.setup_logging()
-
-        return logging.getLogger()
-
     # Dev mode
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
-    handler.setFormatter(formatter)
+    if os.getenv("DEV_MODE"):
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
 
-    logger = logging.getLogger("JobHuntLogger")
-    logger.setLevel(logging.DEBUG)
-    logger.propagate = False
-    logger.addHandler(handler)
+        logger = logging.getLogger("JobHuntLogger")
+        logger.setLevel(logging.DEBUG)
+        logger.propagate = False
+        logger.addHandler(handler)
+        return logger
 
-    return logger
+    # Prod mode
+    client = google.cloud.logging.Client()
+    client.setup_logging()
+
+    return logging.getLogger()
